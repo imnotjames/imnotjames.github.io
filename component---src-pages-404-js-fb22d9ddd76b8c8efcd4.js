e28419b8282d7bbf9717ca7c62044e0d94a02389
imnotjames <james@notjam.es>
2021-02-20 06:12:11 +0000

    Deploying to gh-pages from  @ fe5dd8a8a830ff438c0f9661ddf1af75d66924dc 🚀

---
+++ component---src-pages-404-js-fb22d9ddd76b8c8efcd4.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(t,e,n){"use strict";n.r(e),n.d(e,"default",(function(){return c}));n("q1tI");var a=n("Bl7J"),o=n("vrFN"),i=n("qKvR");function c(t){var e=t.data.site.siteMetadata.title,n=t.location;return Object(i.b)(a.a,{location:n,title:e},Object(i.b)(o.a,{title:"404: Not Found"}),Object(i.b)("h1",{style:{textAlign:"center"}},"(page not found)"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-fb22d9ddd76b8c8efcd4.js.map
