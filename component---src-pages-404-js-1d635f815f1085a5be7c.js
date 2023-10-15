05945cbcf9924e92c62d75ad8fa662a9e29deb8d
imnotjames <james@notjam.es>
2023-10-15 01:25:21 +0000

    Deploying to gh-pages from @ imnotjames/imnotjames.github.io@6ade1baabf1f31f6d75d57eb0cc764cba36f0422 🚀

---
+++ component---src-pages-404-js-1d635f815f1085a5be7c.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknotjam_es=self.webpackChunknotjam_es||[]).push([[883],{429:function(t,e,n){n.r(e),n.d(e,{default:function(){return c}});var a=n(7294),l=n(8678),o=n(9357);function c(t){let{data:{site:{siteMetadata:{title:e}}},location:n}=t;return a.createElement(l.Z,{location:n,title:e},a.createElement(o.Z,{title:"404: Not Found"}),a.createElement("h1",{style:{textAlign:"center"}},"(page not found)"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-1d635f815f1085a5be7c.js.map
